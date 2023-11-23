a052c93991d7d239435928f11aa0e9c1a7f63dfa
Anas Firly <anasfirly20@gmail.com>
2023-11-23 14:54:59 +0700

    feat: adding/setting up react hook form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,21 +4,68 @@
 import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
 
+// React Hook Form
+import { useForm, SubmitHandler } from "react-hook-form";
+
 // Miscellaneous
 
+type TInputs = {
+  fullName: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+};
+
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<TInputs>();
+  const onSubmit: SubmitHandler<TInputs> = (data) => console.log(data);
+
   return (
     <section className="min-h-screen pt-[5rem]">
       <h1 className="text-xl text-center">Registration Form</h1>
-      <form action="" className="grid grid-cols-2 gap-x-20 gap-y-10 mt-16">
-        <Input type="fullName" variant="underlined" label="Full Name" />
-        <Input type="email" variant="underlined" label="Email" />
-        <Input type="password" variant="underlined" label="Password" />
-        <Input type="password" variant="underlined" label="RepeatPassword" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid grid-cols-2 gap-x-20 gap-y-10 mt-16"
+      >
+        <Input
+          type="text"
+          variant="underlined"
+          label="Full Name"
+          {...register("fullName", {
+            required: true,
+          })}
+        />
+        <Input
+          type="email"
+          variant="underlined"
+          label="Email"
+          {...register("email")}
+        />
+        <Input
+          type="password"
+          variant="underlined"
+          label="Password"
+          {...register("password")}
+        />
+        <Input
+          type="password"
+          variant="underlined"
+          label="RepeatPassword"
+          {...register("repeatPassword")}
+        />
+        <Button
+          type="submit"
+          color="primary"
+          className="mt-10 flex justify-center items-center w-full col-span-2"
+        >
+          Sign Up
+        </Button>
       </form>
-      <Button color="primary" className="mt-20 flex w-[30%] mx-auto">
-        Sign Up
-      </Button>
     </section>
   );
 }
